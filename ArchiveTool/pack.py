d9b5a65c8ca41d6d2509d2c7b598a36e13f66ce1
Sam <itsthatguyagain3@gmail.com>
2021-09-07 17:14:39 +1000

    PackFiles.bat and pack.py now only use 2 arguments, third one removed and replaced with os.path.getdirname stuff :D

--- ArchiveTool/pack.py
+++ ArchiveTool/pack.py
@@ -3,19 +3,29 @@ from sys import argv
 import zlib
 import struct
 from pathlib import Path
+import os
 
 print("\nArchive Packer version 0.2\n")
 
 magic = 0xD1AB011CA1
 version = 2
 path_limit = 260
-if len(argv) != 4:
+
+current_size = 0
+total_uncompressed_size = 0
+total_compressed_size = 0
+number_of_compressed_files = 0
+compress_ratio_total = 0
+file_descriptors = []
+
+if len(argv) != 3:
     print("\nArguments are : pack.py \\folder\\**\\*.* [output.extension]\n")
     exit()
 
 file_list = glob.glob(argv[1], recursive=True)
-file_list = [x.replace("\\", "/") for x in file_list if x.find('.')
-             != -1 and Path(x).stat().st_size != 0]
+file_list = [x for x in file_list if x.find('.') != -1]
+file_list = [x for x in file_list if Path(x).stat().st_size != 0]
+file_list = [x.replace("\\", "/") for x in file_list]
 number_of_files = len(file_list)
 
 if number_of_files == 0:
@@ -24,10 +34,7 @@ if number_of_files == 0:
     exit()
 
 header = struct.pack("QQQ", magic, version, number_of_files)
-file_descriptors = []
-current_size = 0
-total_uncompressed_size = 0
-total_compressed_size = 0
+
 
 class Descriptor():
     def __init__(self, name, uncompressed_size, size, data, is_compressed):
@@ -39,9 +46,9 @@ class Descriptor():
         self.is_compressed = is_compressed
         self.data = data
 
-number_of_compressed_files = 0
+
 print(f"Packing {number_of_files} files into archive\n")
-compress_ratio_total = 0
+
 for file in file_list:
     with open(file, "rb") as f:
         data = f.read()
@@ -53,34 +60,46 @@ for file in file_list:
         compress_ratio = (data_compressed_size / data_uncompressed_size) * 100
         is_compressed = True
         if compress_ratio < 100.0:
+
             compress_ratio_total += compress_ratio
-            print(f"Adding and Compressing {file} at compress ratio {compress_ratio}")
+            print(f"Adding Compresssed {file}\
+            at compress ratio {compress_ratio}")
             data = data_compressed
             data_size = data_compressed_size
             number_of_compressed_files += 1
             total_compressed_size += data_compressed_size
+
         else:
             print(f"Adding Uncompressed {file}...")
             data_size = data_uncompressed_size
             is_compressed = False
             total_compressed_size += data_uncompressed_size
 
-        fdesc = Descriptor(
-            bytearray(file[-(len(file)-len(argv[3])):], "ASCII"), data_uncompressed_size, data_size, data, is_compressed)
+        file_name = bytearray(
+            file[-(len(file) - len(os.path.dirname(argv[1])[:-2])):], "ASCII")
+        file_desc = Descriptor(
+            file_name, data_uncompressed_size, data_size, data, is_compressed)
+
         if data_size == 0 or data_uncompressed_size == 0:
             print(f"error {file} empty!")
             exit()
-        file_descriptors.append(fdesc)
-        f.close()
+
+        file_descriptors.append(file_desc)
         current_size += data_size
+        f.close()
 
 descriptor_data = bytearray()
 archive_data = bytearray()
-print(f"\nBuilding archive {argv[2]} ({number_of_files} files, {number_of_compressed_files} compressed)")
+
+print(
+    f"\nBuilding archive {argv[2]} ({number_of_files}"
+    " files, {number_of_compressed_files} compressed)")
+
 for fd in file_descriptors:
     if len(fd.name) > path_limit:
         print(f"error file name too long! - {fd.name}")
         exit()
+
     descriptor_data.extend(fd.name)
     descriptor_data.extend(b"\x00" * (path_limit - len(fd.name)))
     descriptor_data.extend(b"\x00" * 4)  # padding
@@ -93,5 +112,10 @@ with open(argv[2], "wb") as f:
     f.write(archive)
     f.close()
 print(f"Done! Archive {argv[2]} is written to file")
-print(f"Compression Ratio Average {compress_ratio_total / number_of_compressed_files}%")
-print(f"Total Compression Ratio {total_compressed_size / total_uncompressed_size * 100}% (Saved {total_uncompressed_size - total_compressed_size} bytes)")
+print(
+    f"Compression Ratio Average \
+    {compress_ratio_total / number_of_compressed_files}%")
+print(
+    f"Total Compression Ratio \
+    {total_compressed_size / total_uncompressed_size * 100}\
+    % (Saved {total_uncompressed_size - total_compressed_size} bytes)")
